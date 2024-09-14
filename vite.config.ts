a958093161c8e5f68e85fc50783f001cc4e85e0d
jopaleti <oopaletijohnson@gmail.com>
2024-09-14 15:10:34 +0100

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend/",
   plugins: [react()],
-  build: {
-    outDir: "dist", // Output folder for the production build
-  },
 });
